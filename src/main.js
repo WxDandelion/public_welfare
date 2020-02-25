adb5cc50ebe172e766c2456471565896af1fce8e
wxdandelion <wxbyr@bupt.edu.cn>
2020-02-25 22:42:39 +0800

    一些修改

--- src/main.js
+++ src/main.js
@@ -7,10 +7,6 @@ import VueRouter from 'vue-router';
 import App from './App'
 import router from './router'
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(ElementUI)
 
 Vue.use(VueRouter);
 Vue.use(iView);
